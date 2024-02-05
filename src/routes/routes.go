678e84382c670e60e82bd78cbfdc414fd48596f7
takazo3 <info@re-zero-jp.com>
2024-02-05 23:04:21 +0900

    Update Link model to omit empty Orders field

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -41,7 +41,11 @@ func Setup(app *fiber.App) {
 	ambassadorAuthenticated.Put("users/info", controllers.UpdateInfo)
 	ambassadorAuthenticated.Put("users/password", controllers.UpdatePassword)
 	ambassadorAuthenticated.Post("links", controllers.CreateLink)
-	
+	ambassadorAuthenticated.Get("stats", controllers.Stats)
 	ambassadorAuthenticated.Get("rankings", controllers.Rankings)
+
+	checkout := api.Group("checkout")
+	checkout.Get("links/:code", controllers.GetLink)
+
 
 }
